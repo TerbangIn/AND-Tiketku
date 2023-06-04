9b65b107e30e1f57fa8bc999b325918fa236d163
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-06-04 16:29:04 +0700

    change package

---
+++ app/src/main/java/com/ketarketir/tiketkuioflight/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ketarketir.tiketkuioflight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
