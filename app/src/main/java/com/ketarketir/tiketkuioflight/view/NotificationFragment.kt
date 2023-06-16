85211a7c9b2df4d609b32573de95f717301745bf
SekarAisyah <sekarrisma@apps.ipb.ac.id>
2023-06-16 18:46:56 +0700

    Completed Notification Fragment

--- app/src/main/java/com/ketarketir/tiketkuioflight/view/NotificationFragment.kt
+++ app/src/main/java/com/ketarketir/tiketkuioflight/view/NotificationFragment.kt
@@ -2,13 +2,11 @@ package com.ketarketir.tiketkuioflight.view
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ketarketir.tiketkuioflight.databinding.FragmentNotificationBinding
 import com.ketarketir.tiketkuioflight.viewmodel.NotificationViewModel
