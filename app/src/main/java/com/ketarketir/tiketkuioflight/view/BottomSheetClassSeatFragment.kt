5834d645c52446f64556e7206c0ce08433157eba
Pucukio <pucuk4us@gmail.com>
2023-06-19 14:03:58 +0700

    Fixed Localization EN - INA

---
+++ app/src/main/java/com/ketarketir/tiketkuioflight/view/BottomSheetClassSeatFragment.kt
@@ -0,0 +1,33 @@
+package com.ketarketir.tiketkuioflight.view
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ketarketir.tiketkuioflight.R
+import com.ketarketir.tiketkuioflight.viewmodel.BottomSheetClassSeatViewModel
+
+class BottomSheetClassSeatFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = BottomSheetClassSeatFragment()
+    }
+
+    private lateinit var viewModel: BottomSheetClassSeatViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_bottom_sheet_class_seat, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(BottomSheetClassSeatViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/ketarketir/tiketkuioflight/viewmodel/BottomSheetPaymentSuccessViewModel.kt
@@ -0,0 +1,7 @@
+package com.ketarketir.tiketkuioflight.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class BottomSheetPaymentSuccessViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
