7c2304f9bb7e19c7a883d91f76d9066ae62b10f7
Rio Griya Putra <Pucuk4us@gmail.com>
2023-06-22 20:02:25 +0700

    Fixed PaymentFragment

---
+++ app/src/main/java/com/ketarketir/tiketkuioflight/view/PaymentFragment.kt
@@ -0,0 +1,33 @@
+package com.ketarketir.tiketkuioflight.view
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ketarketir.tiketkuioflight.R
+import com.ketarketir.tiketkuioflight.viewmodel.PaymentViewModel
+
+class PaymentFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PaymentFragment()
+    }
+
+    private lateinit var viewModel: PaymentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_payment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(PaymentViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/ketarketir/tiketkuioflight/viewmodel/PaymentViewModel.kt
@@ -0,0 +1,7 @@
+package com.ketarketir.tiketkuioflight.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class PaymentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
