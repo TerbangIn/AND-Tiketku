b0d7042067343711d2ca6ae2d33f5a3e3b0337d2
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-06-25 21:44:53 +0700

    modified

--- app/src/main/java/com/ketarketir/tiketkuioflight/view/BottomSheetHomeSearchDestinationFragment.kt
+++ app/src/main/java/com/ketarketir/tiketkuioflight/view/BottomSheetHomeSearchDestinationFragment.kt
@@ -28,7 +28,6 @@ class BottomSheetHomeSearchDestinationFragment : Fragment() {
     private lateinit var binding: FragmentBottomSheetHomeSearchDestinationBinding
     private lateinit var viewModel: BottomSheetHomeSearchDestinationViewModel
     private lateinit var userManager: UserManager
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,62 +38,22 @@ class BottomSheetHomeSearchDestinationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this).get(BottomSheetHomeSearchDestinationViewModel::class.java)
+        viewModel =
+            ViewModelProvider(this).get(BottomSheetHomeSearchDestinationViewModel::class.java)
         userManager = UserManager.getInstance(requireContext())
 
         showAirport()
 
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
 
-
-//        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
-//            override fun onQueryTextSubmit(query: String?): Boolean {
-////                viewModel.getToken()
-////                viewModel.token.observe(viewLifecycleOwner, Observer {
-////                    val token = it
-////                    viewModel.callApiListAirport(token)
-////                    viewModel.listAirport.observe(viewLifecycleOwner, Observer {
-////                        val data = it.toString()
-////                        if (it!= null){
-////                            if (data.contains(query.orEmpty(), ignoreCase = true)){
-////                                val adapter : ArrayAdapter<Char> = ArrayAdapter(
-////                                    requireContext(),
-////                                    android.R.layout.simple_list_item_1,
-////                                    data.toList()
-////                                )
-////                                adapter.filter.filter(query)
-////                            }
-////                        } else {
-////                            Toast.makeText(context, "Airport Tidak ditemukan", Toast.LENGTH_SHORT).show()
-////                        }
-////                    })
-//                })
-////                return false
-//            }
-//
-//            override fun onQueryTextChange(newQuery: String?): Boolean {
-////                viewModel.getToken()
-////                viewModel.token.observe(viewLifecycleOwner, Observer {
-////                    val token = it
-////                    viewModel.callApiListAirport(token)
-////                    viewModel.listAirport.observe(viewLifecycleOwner, Observer {
-////                        val data = it.toString()
-////                        if (it!= null){
-////                            if (data.contains(newQuery.orEmpty(), ignoreCase = true)){
-////                                val adapter : ArrayAdapter<Char> = ArrayAdapter(
-////                                    requireContext(),
-////                                    android.R.layout.simple_list_item_1,
-//////                                    data.toList()
-////                                )
-////                                adapter.filter.filter(newQuery)
-////                            }
-////                        } else {
-////                            Toast.makeText(context, "Airport Tidak ditemukan", Toast.LENGTH_SHORT).show()
-////                        }
-////                    })
-////                })
-////                return false
-////            }
-//        })
+            override fun onQueryTextChange(newQuery: String?): Boolean {
+                performSearch(newQuery.toString())
+                return true
+            }
+        })
     }
 
     fun showAirport(){
@@ -107,6 +66,22 @@ class BottomSheetHomeSearchDestinationFragment : Fragment() {
                     val adapter = AirportAdapter(it)
                     binding.rvRecentSearch.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
                     binding.rvRecentSearch.adapter = adapter
+                } else{
+                    Toast.makeText(context, "null", Toast.LENGTH_SHORT).show()
+                }
+            })
+        })
+    }
+
+    private fun performSearch(query: String) {
+        viewModel.getToken()
+        viewModel.token.observe(viewLifecycleOwner, Observer {
+            val token = it
+            viewModel.callApiListAirport(token)
+            viewModel.listAirport.observe(viewLifecycleOwner, Observer {
+                if (it!= null){
+                    val adapter = AirportAdapter(it)
+                    adapter.filterAirportList(query)
                 } else{
                     Toast.makeText(context, "null", Toast.LENGTH_SHORT).show()
                 }
